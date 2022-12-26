4dcab90acee2453972bf1707a4ba25645f515b65
KSENAK_NTB\JanKsenak <jan.ksenak@gmail.com>
2022-12-26 18:27:44 +0100

    Add env file and variables

--- astro/src/scripts/index.js
+++ astro/src/scripts/index.js
@@ -1,3 +1,12 @@
+'use strict'
+
+const host = import.meta.env.HOST;
+const port = import.meta.env.PORT;
+
+export const hostURL = 'http://' + host + ':' + port + '/'
+
+
+
 export const bgColorList = ['bg-pink-100 text-pink-800','bg-fuchsia-100 text-fuchsia-800','bg-purple-100 text-purple-800','bg-indigo-100 text-indigo-800','bg-blue-100 text-blue-800','bg-sky-100 text-sky-800','bg-emerald-100 text-emerald-800','bg-green-100 text-green-800','bg-yellow-100 text-yellow-800','bg-orange-100 text-orange-800','bg-red-100 text-amber-800', 'bg-red-100 text-amber-800', 'bg-teal-100 text-teal-800', 'bg-lime-100 text-lime-800', 'bg-cyan-100 text-cyan-800', 'bg-rose-100 text-rose-800']
 
 export function getRandomItem(List){
