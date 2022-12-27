a92312d2f85b2992774cf7544d81d03d31fabd63
KSENAK_NTB\JanKsenak <jan.ksenak@gmail.com>
2022-12-27 12:25:29 +0100

    db.js update

--- strapi/config/env/production/database.js
+++ strapi/config/env/production/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'mysql',
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'mysql80.r5.websupport.sk'),
         port: env.int('DATABASE_PORT', 3314),
         database: env('DATABASE_NAME', 'personalPageDB'),
         user: env('DATABASE_USERNAME', 'jankseAdmin'),
