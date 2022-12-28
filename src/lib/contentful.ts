4def77e0ab5ee23efefce909774cd22ab0b9007f
KSENAK_NTB\JanKsenak <jan.ksenak@gmail.com>
2022-12-28 11:10:00 +0100

    Connect contentfull with astro

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,4 +1,46 @@
-import contentful from "contentful";
+import contentful, { Asset } from "contentful";
+import type { Document } from "@contentful/rich-text-types";
+
+
+export interface Timeline {
+  title: string;
+  years: string;
+  description: string;
+  isCurrent: boolean;
+  type: string;
+}
+
+export interface SocialMedia {
+  url: string;
+  srLabel: string;
+  type: string;
+}
+
+export interface LandingPage {
+  greetingsTitle: string;
+  bio1title: string;
+  bio2title: string;
+  iAmList: [string];
+  skillsTitle: string;
+  skillsSubtitle1: string;
+  skillsSubtitle2: string;
+  automationSkillList: [string];
+  itSkillList: [string];
+  timelineTitle: string;
+  timeline1subtitle: string;
+  timeline2subtitle: string;
+  resumeTitle: string;
+  contactTitle: string;
+  email: string;
+  skTelNumber: string;
+  czTelNumber: string;
+  skAddress: string;
+  czAddress: string;
+  footerContent: Document;
+  resume: Asset;
+
+}
+
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
